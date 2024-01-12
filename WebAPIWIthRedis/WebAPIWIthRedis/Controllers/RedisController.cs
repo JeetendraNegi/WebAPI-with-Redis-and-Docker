6f12553021491e2cd151aa0d463418451f3e0901
Jeetendra Negi <40231981+JeetendraNegi@users.noreply.github.com>
2024-01-12 11:49:56 +0530

    upload code

---
+++ WebAPIWIthRedis/WebAPIWIthRedis/Controllers/RedisController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace WebAPIWIthRedis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RedisController : ControllerBase
+    {
+        private readonly IDatabase _db;
+
+        public RedisController()
+        {
+            var redis = ConnectionMultiplexer.Connect("redis");
+            _db = redis.GetDatabase();
+        }
+
+        [HttpGet("{key}")]
+        public async Task<IActionResult> Get(string key)
+        {
+            var value = await _db.StringGetAsync(key);
+            return Ok(value.ToString());
+        }
+
+        [HttpPost("{key}")]
+        public async Task<IActionResult> Post(string key, [FromBody] string value)
+        {
+            await _db.StringSetAsync(key, value);
+            return Ok();
+        }
+    }
+}
---
+++ WebAPIWIthRedis/WebAPIWIthRedis/Program.cs
@@ -0,0 +1,44 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddMemoryCache();
+builder.Services.AddSwaggerGen();
+
+// registring httpclient
+builder.Services.AddHttpClient();
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("*");
+                      });
+});
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseCors(MyAllowSpecificOrigins);
+
+app.MapDefaultControllerRoute();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.Run();
